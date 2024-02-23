5d0c90a499910a291a2bd178575b1c3558f5c7d9
tiagornandrade <tiagornandrade@hotmail.com>
2024-02-23 16:35:46 -0300

    feat: Ajusta projeto

--- src/flink_stream_processor.py
+++ src/flink_stream_processor.py
@@ -7,24 +7,20 @@ from pyflink.datastream.connectors import FlinkKafkaConsumer, FlinkKafkaProducer
 def main():
     env = StreamExecutionEnvironment.get_execution_environment()
 
-    properties = {
-        'bootstrap.servers': 'localhost:9092',
-        'group.id': 'flink-group'
-    }
+    properties = {"bootstrap.servers": "localhost:9092", "group.id": "flink-group"}
 
-    kafka_jar = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'flink-sql-connector-kafka_2.12-1.14.6.jar')
+    kafka_jar = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        "flink-sql-connector-kafka_2.12-1.14.6.jar",
+    )
     env.add_jars(f"file:///{kafka_jar}")
 
     kafka_source = FlinkKafkaConsumer(
-        'financial_transactions',
-        SimpleStringSchema(),
-        properties=properties
+        "financial_transactions", SimpleStringSchema(), properties=properties
     )
 
     kafka_sink = FlinkKafkaProducer(
-        'processed_transactions',
-        SimpleStringSchema(),
-        producer_config=properties
+        "processed_transactions", SimpleStringSchema(), producer_config=properties
     )
 
     stream = env.add_source(kafka_source)
@@ -36,5 +32,5 @@ def main():
     env.execute("Kafka Flink Python Example")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
